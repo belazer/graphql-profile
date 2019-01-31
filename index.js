3b3997e5575a18195cacaed078fbff65cb2d364a
Jonas Faber <jonas@graphcms.com>
2019-01-31 18:30:26 +0100

    New performance metrics and config support

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-import { GraphQLClient } from 'graphql-request'
+import { GraphQLClient } from 'graphql-request';
+let config = process.argv[2]
+if(!config){
+  console.log(`pass a relative path to a config file`);
+  process.exit(0)
+}
+console.log(`Welcome to a fun profiling! Config(${config})`);
 
-console.log(`Welcome to a fun profiling!`);
+const {
+  QUERY,
+  API,
+  TOKEN,
+  ITERATIONS,
+  PARALLEL,
+} = require(config)
 
-const API = process.env.GRAPHQL_API || 'insert api link here';
-const TOKEN = process.env.GRAPHQL_TOKEN || 'insert token here';
-const ITERATIONS = 500;
 
+
+let max = Number.MIN_SAFE_INTEGER;
+let min = Number.MAX_SAFE_INTEGER;
+let times = [];
+let errors = [];
 const client = new GraphQLClient(API, {
   headers: {
     Authorization: `Bearer ${TOKEN}`,
@@ -14,34 +28,51 @@ const client = new GraphQLClient(API, {
 
 let profiledRequest = (...args) => {
   let start = new Date();
-  return client.request(...args).then(data => {
-    let end = new Date();
-    console.log(`${start.toISOString()}, ${end.toISOString()}, ${end.getTime() - start.getTime()}`);
-    return data;
-  })
-};
-
-const query = `
-    query pointlessQuery {
-      __type(name: "ID") {
-        name
+  return client
+    .request(...args)
+    .then(data => {
+      let end = new Date();
+      let duration = end.getTime() - start.getTime();
+      if (duration < min) {
+        min = duration;
       }
-    }`;
-
-const superSimpleQuery = async () => {
-  return profiledRequest(query);
+      if (duration > max) {
+        max = duration;
+      }
+      times.push(duration);
+      return data;
+    })
+    .catch(e => {
+      errors.push(e);
+    });
 };
 
-console.log(`Running with query: ${query}`);
+
+
+console.log(`Running with query: ${QUERY}`);
 console.log(`Profiling: ${API}`);
 console.log(`Using API Key: ${TOKEN}`);
 
-console.log(`startTime, endTime, duration(mS)`);
-
 const runTest = async () => {
-  for(let i = 0; i < ITERATIONS; i++) {
-    await superSimpleQuery();
+  for (let i = 0; i < ITERATIONS; i++) {
+    try {
+      await Promise.all(Array(PARALLEL).fill(profiledRequest(QUERY)));
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
+async function main() {
+  try {
+    await runTest();
+    let msg = `Max: ${max}ms
+Mean: ${times.reduce((acc, v) => acc + v, 0) / times.length}ms
+Min ${min}ms
+Errors ${errors.length}/${PARALLEL * ITERATIONS}`;
+    console.log(msg);
+  } catch (err) {
+    console.log(`Caught an error while profiling: ${err} ${err.stack}`);
+  }
+}
 
-runTest().catch(err => console.log(`Caught an error while profiling: ${err} ${err.stack}`));
+main();
